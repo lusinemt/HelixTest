c347deab85f9e0fa2da97f1e64d554ad9fc4e338
lusinemt <35559053+lusinemt@users.noreply.github.com>
2018-08-22 12:44:42 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/update/cities', 'UpdateCitiesController@txtToArray');
-
 Route::get('/autocomplete', 'SearchFormController@index');
 
 Route::get('/autocomplete/search', [
